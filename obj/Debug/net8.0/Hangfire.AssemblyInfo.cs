ce4197fdc5bac63fc724fc4ead84178b8b86d27b
tihomir vanjurek <tihomir.vanjurek@gmail.com>
2025-02-05 19:40:19 +0100

    test

--- obj/Debug/net8.0/Hangfire.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hangfire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangfire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c038ca6102bbfcc26983291389527f870d768e2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12908c91278efb4591e885f01da967b71140d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangfire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangfire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
